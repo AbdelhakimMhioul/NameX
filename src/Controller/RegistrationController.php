eda907b92286c175a5e61101e56e7c434057fdcb
AbdelhakimMhioul <hk.mhl1975@gmail.com>
2022-05-22 21:09:44 +0100

    App Finished

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -5,7 +5,6 @@ namespace App\Controller;
 use App\Entity\User;
 use App\Form\RegistrationFormType;
 use App\Security\EmailVerifier;
-use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
